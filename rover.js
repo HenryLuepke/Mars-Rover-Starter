c9c067f9746614d8f907626ea5a071eb4d8eb9df
HenryLuepke <henryluepke4@gmail.com>
2024-07-24 14:05:26 -0500

    Rover functionality and test updates

--- rover.js
+++ rover.js
@@ -1,5 +1,52 @@
 class Rover {
-   // Write code here!
+   constructor() {
+      this.position = 'position';
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+
+   move(message, i) {
+      if (this.mode === 'NORMAL') {
+         //this.position = message.commands[i].value
+         return {completed: true}
+      } else {
+         return {completed: false}
+      }
+   }
+
+   statusCheck() {
+      return {
+         completed: true,
+         roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}
+      }
+   }
+
+   modeChange(message, i) {
+      this.mode = message.commands[i].value
+      return {completed: true}
+   }
+
+   recieveMessage(message) {
+      let output = {
+         message: message.name,
+         results: []
+      }
+      for (let i = 0; i < message.commands.length; i++) {
+         if (message.commands[i].commandType === 'MOVE') {
+            output.results.push(this.move(message, i))
+         } else if (message.commands[i].commandType === 'STATUS_CHECK') {
+            output.results.push(this.statusCheck(message))
+         } else if (message.commands[i].commandType === 'MODE_CHANGE') {
+            output.results.push(this.modeChange(message, i))
+         } else {
+            throw Error("Invalid command");
+         }
+      }
+
+      return output
+
+   }
+
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -15,38 +15,40 @@ describe("Rover class", function() {
   })
 
   it("response returned by receiveMessage contains the name of the message", function() {
-    let newMessage = new Message("name of message", [new Command("command1", "command1 type"), new Command("command2", "command2 type"), new Command("command3", "command3 type")])
+    let newMessage = new Message("name of message", [new Command("MOVE", 100)])
     expect(new Rover().recieveMessage(newMessage).message).toBe('name of message')
   })
 
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
-    let newMessage = new Message("name of message", [new Command("command1", "command1 type"), new Command("command2", "command2 type")])
+    let newMessage = new Message("name of message", [new Command("MOVE", 100), new Command("STATUS_CHECK")])
     expect(new Rover().recieveMessage(newMessage).results.length).toBe(2)
   })
 
   it("responds correctly to the status check command", function() {
     let newMessage = new Message("status check", [new Command("STATUS_CHECK")])
     let rover = new Rover()
-    expect(rover.recieveMessage(newMessage).results.RoverStatus).toBe({roverStatus: {mode: rover.mode, generatorWatts: rover.generatorWatts, position: rover.position}})
+    expect(rover.recieveMessage(newMessage).results[0].roverStatus).toBe({roverStatus: {mode: rover.mode, generatorWatts: rover.generatorWatts, position: rover.position}})
   })
 
   it("responds correctly to the mode change command", function() {
     let newMessage = new Message("mode change", [new Command("MODE_CHANGE", "LOW_POWER")])
-    let rover = new Rover().recieveMessage(newMessage)
-    expect(rover.completed).toBe(true)
+    let rover = new Rover()
+    let roverOutput = rover.recieveMessage(newMessage)
+    expect(roverOutput.results[0].completed).toBe(true)
     expect(rover.mode).toBe("LOW_POWER")
   })
 
   it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
     let newMessage = new Message("mode change and move attempt", [new Command("MODE_CHANGE", "LOW_POWER"), new Command("MOVE", 100)])
-    let rover = new Rover(50).recieveMessage(newMessage)
-    expect(rover.completed).toBe(false)
-    expect(rover.position).toBe(50)
+    let rover = new Rover()
+    let roverOutput = rover.recieveMessage(newMessage)
+    expect(roverOutput.results[1].completed).toBe(false)
+    expect(rover.position).toBe('position')
   })
 
   it("responds with the position for the move command", function() {
     let moveMessage = new Message("move", [new Command("MOVE", 100)])
-    let rover = new Rover(50).recieveMessage(moveMessage)
+    let rover = new Rover().recieveMessage(moveMessage)
     expect(rover.position).toBe(100)
   })
 
